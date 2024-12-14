403b779f557006e1e3f3ffb1d5c0a152628b4014
Shizukana Otaku <otakushizukana@gmail.com>
2024-12-14 15:53:21 +0000

    Fix rustup install

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 sudo pacman -S --needed base-devel git --noconfirm
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup default stable
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
 rm -rf paru
 paru -S rustup --needed --noconfirm
-rustup default stable
 git clone https://github.com/comtrya/comtrya
 cd comtrya
 cargo build --release --all-features
