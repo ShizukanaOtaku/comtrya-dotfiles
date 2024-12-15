7eaf00942e596e87bb83035536e8ba78605719f0
Shizukana Otaku <otakushizukana@gmail.com>
2024-12-15 13:16:29 +0000

    Parallelize the installation script

--- install.sh
+++ install.sh
@@ -4,19 +4,25 @@ sudo pacman -S --needed base-devel git rustup --noconfirm
 rustup default stable
 
 # Paru installation
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-cd ..
-rm -rf paru
+(
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si
+  cd ..
+  rm -rf paru
+) &
 
 # Comtrya (git version)
-git clone https://github.com/comtrya/comtrya
-cd comtrya
-cargo build --release --all-features
-sudo mv target/release/comtrya /usr/bin/comtrya
-cd ..
-rm -rf comtrya
+(
+  git clone https://github.com/comtrya/comtrya
+  cd comtrya
+  cargo build --release --all-features
+  sudo mv target/release/comtrya /usr/bin/comtrya
+  cd ..
+  rm -rf comtrya
+) &
+
+wait
 
 # Clone and install the dotfiles
 git clone https://github.com/ShizukanaOtaku/comtrya-dotfiles
