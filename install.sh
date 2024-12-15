31d2e426ea009a65d83ce72e09f175ce1758f7e9
Shizukana Otaku <otakushizukana@gmail.com>
2024-12-15 11:42:33 +0000

    Fix minor issues

--- install.sh
+++ install.sh
@@ -1,17 +1,29 @@
+#!/bin/bash
+
 sudo pacman -S --needed base-devel git --noconfirm
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup default stable
+if ! command -v cargo 2>&1 >/dev/null
+then
+  echo "Cargo could not be found!"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  rustup default stable
+fi
+
+# Paru installation
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
 rm -rf paru
+
+# Comtrya (git version)
 git clone https://github.com/comtrya/comtrya
 cd comtrya
 cargo build --release --all-features
 sudo mv target/release/comtrya /usr/bin/comtrya
 cd ..
 rm -rf comtrya
+
+# Clone and install the dotfiles
 git clone https://github.com/ShizukanaOtaku/comtrya-dotfiles
 cd comtrya-dotfiles
-comtrya -v -d . apply
+sudo comtrya -v -d . apply
